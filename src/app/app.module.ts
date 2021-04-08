c9f83f5a5feb7f3b2d569f72897955ab73b8fb5d
caiomyrapereira <caiomyrapereira@hotmail.com>
2021-04-08 13:11:36 -0300

    Separando melhor os components por tipo de nomenclaturas de cada diretório.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,13 @@ import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/layout/navbar/navbar.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
-import { ButtonComponent } from './components/button/button.component';
-import { CardComponent } from './components/card/card.component';
-import { BottomBannerComponent } from './components/bottom-banner/bottom-banner.component';
-import { TopBannerComponent } from './components/top-banner/top-banner.component';
+import { ButtonComponent } from './components/utilities/button/button.component';
+import { CardComponent } from './components/utilities/card/card.component';
+import { BottomBannerComponent } from './components/layout/bottom-banner/bottom-banner.component';
+import { TopBannerComponent } from './components/layout/top-banner/top-banner.component';
 
 @NgModule({
   declarations: [
