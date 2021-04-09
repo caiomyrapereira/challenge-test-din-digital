fb589437a8df4a55d3165456ba55682434d3191b
caiomyrapereira <caiomyrapereira@hotmail.com>
2021-04-09 18:20:24 -0300

    Corrigindo o compoente 'card' para trabalhar com slots -via 'ng-content'- do que com as propriedades de @Input.

--- src/app/components/utilities/card/card.component.ts
+++ src/app/components/utilities/card/card.component.ts
@@ -6,11 +6,6 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit {
-  @Input() id: string = '';
-  @Input() title: string = '';
-  @Input() subtitle: string = '';
-  @Input() description: string = '';
-  @Input() image: string = '';
   @Input() cardStyle: string ='';
 
   constructor() { }
